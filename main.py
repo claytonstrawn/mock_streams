248a9c7a68a10b933ec42e87bfbcfa0ebde1a0a3
Clayton Strawn <cjstrawn@ucsc.edu>
2021-07-01 16:52:01 -0700

    added setup function and made importable

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ def main_function(geo_args, phys_args):
     ds = yt_section.convert_to_dataset(fields)
     return ds
 
+def do_setup(n=50,box_size = 200):
+    max_size = box_size/2
+    x_vals = np.linspace(-max_size,max_size,n)
+    y_vals = np.linspace(-max_size,max_size,n)
+    z_vals = np.linspace(-max_size,max_size,n)
+
+    xs = np.tile(x_vals,(n,n,1)).transpose((2,1,0))
+    ys = np.tile(y_vals,(n,n,1)).transpose((0,1,2))
+    zs = np.tile(z_vals,(n,n,1)).transpose((1,2,0))
+    return xs,ys,zs
+
 #geometry section 
 #code leader: Parsa
 def identify_phases():
